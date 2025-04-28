148005b06dbf8a675a62f9887dbd9e4441e1f00c
Ian Titus Glover <iantitusglover2@gmail.com>
2025-04-28 16:33:24 +0000

    installing tailwind.css and setting up database using mongodb

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,13 +1,27 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+import EditTodo from './components/EditTodo';
+import './index.css';
 
 function App() {
   return (
-<div className="App">
-     <h1>hello</h1>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <div className="container mx-auto px-4 py-8 max-w-3xl">
+          <Routes>
+            <Route path="/" element={<TodoList />} />
+            <Route path="/create" element={<CreateTodo />} />
+            <Route path="/edit/:id" element={<EditTodo />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
+
 export default App;
-    
